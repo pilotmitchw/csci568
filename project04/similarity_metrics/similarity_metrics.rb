74bf4d9acacd336ec01d78421352083ae8f640ab
Mitchell Wright <pilotmitchw@gmail.com>
2011-09-18 17:21:53 -0600

    Initial commit - ruby source file

---
+++ project04/similarity_metrics/similarity_metrics.rb
@@ -0,0 +1,20 @@
+class EuclideanDistance
+
+end
+
+class SimpleMatchingCoefficient
+
+end
+
+class JaccardSimilarity
+
+end
+
+class PearsonCorrelationCoefficient
+
+end
+
+class CosineSimilarity
+
+end
+
