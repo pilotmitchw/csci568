da21858c33328741a4d4acd52ed2454fa7df402a
Mitchell Wright <pilotmitchw@gmail.com>
2011-09-19 19:53:14 -0600

    Implemented cosine similarity, working on Pearson

--- project04/similarity_metrics/similarity_metrics.rb
+++ project04/similarity_metrics/similarity_metrics.rb
@@ -74,13 +74,69 @@ end
 
 class PearsonCorrelationCoefficient
 	def distance(x,y)
-	
+		#Dimensionality
+		xDimensions = x.attributes.length
+		yDimensions = y.attributes.length
+		
+		#No point in comparing objects with different dimensionalities
+		if xDimensions != yDimensions 
+			return nil
+		end
+		
+		total = 0
+		
+		x_mean = 0
+		y_mean = 0
+		
+		xDimensions.times do |i|
+			x_mean += x.attributes[i]
+			y_mean += y.attributes[i]
+		end
+		
+		x_mean = x_mean / xDimensions
+		y_mean = y_mean / yDimensions
+		
+		stdev_x = 0
+		stdev_y = 0
+		
+		xDimensions.times do |i|
+			stdev_x += (x.attributes[i] - x_mean)**2
+			stdev_y += (y.attributes[i] - y_mean)**2
+		end
+		
+		stdev_x = Math.sqrt(stdev_x/(xDimensions - 1))
+		stdev_y = Math.sqrt(stdev_y/(yDimensions - 1))
+		
+		
 	end
 end
 
 class CosineSimilarity
 	def distance(x,y)
-	
+		#Dimensionality
+		xDimensions = x.attributes.length
+		yDimensions = y.attributes.length
+		
+		#No point in comparing objects with different dimensionalities
+		if xDimensions != yDimensions 
+			return nil
+		end
+		
+		total = 0
+		x_dot_y = 0
+		x_dot_x = 0
+		y_dot_y = 0
+		
+		xDimensions.times do |i|
+			x_dot_y += x.attributes[i] * y.attributes[i]
+			x_dot_x += x.attributes[i] * x.attributes[i]
+			y_dot_y += y.attributes[i] * y.attributes[i]
+		end
+		
+		x_dot_x = Math.sqrt(x_dot_x)
+		y_dot_y = Math.sqrt(y_dot_y)
+		
+		return x_dot_y.to_f / (x_dot_x * y_dot_y)
 	end
 end
 
@@ -96,7 +152,9 @@ same_two = DataObject.new([1, 0])
 e = EuclideanDistance.new
 smc = SimpleMatchingCoefficient.new
 jaccard = JaccardSimilarity.new
+cosine = CosineSimilarity.new
 
 puts "Euclidian: #{e.distance(one, two)}"
 puts "SMC: #{smc.distance(same_one, same_two)}"
 puts "Jaccard: #{jaccard.distance(same_one, same_two)}"
+puts "Cosine similarity: #{cosine.distance(one, two)}"
