b3152809798de329fd39e7f652d0f10e89a2767e
Mitchell Wright <pilotmitchw@gmail.com>
2011-09-28 20:11:12 -0600

    Commit end of day.

--- project05/kmeans.rb
+++ project05/kmeans.rb
@@ -5,6 +5,28 @@ class DataObject
 	end
 end
 
+class EuclideanDistance
+	def distance(x, y)
+		#Dimensionality
+		xDimensions = x.attributes.length
+		yDimensions = y.attributes.length
+		
+		#No point in comparing objects with different dimensionalities
+		if xDimensions != yDimensions 
+			return nil
+		end
+		
+		total = 0
+		
+		xDimensions.times do |i|
+			total += (x.attributes[i] - y.attributes[i])**2
+		end
+		
+		return Math.sqrt(total)
+	end
+end
+
+
 class Centroid
 	attr_accessor :location
 	def initialize(location)
@@ -27,6 +49,10 @@ def print_all(data_objects)
 	data_objects.each do |i|
 		puts "Object: sepal length = #{i.attributes[0]}, sepal width = #{i.attributes[1]}, petal length = #{i.attributes[2]}, petal width = #{i.attributes[3]}, class = #{i.attributes[4]}"
 	end
+end
+
+def k_means_clustering (k, data_objects)
+	
 end
 
 data_objects = read_file("iris.csv")
