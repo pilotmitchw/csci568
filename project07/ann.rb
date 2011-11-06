12067b84a3bf8948d45b260f5c2b447ab4c6847c
Mitchell Wright <pilotmitchw@gmail.com>
2011-11-05 22:30:23 -0600

    Finished milestone 1 - OOP structure of ANN

--- project07/ann.rb
+++ project07/ann.rb
@@ -4,29 +4,63 @@ require_relative 'neuron'
 
 #ANN - models the entire neural network
 class ANN
-
+	
+	def random_weight
+		result = Random.rand
+		sign = Random.rand(1)
+		
+		if(sign == 0)
+			result = (-1.0)*result
+		end
+		
+		return result
+	end
+	
 	def initialize
-		#Try a perceptron first, the one in the book, page 247
+		@layers = []
 		
-		#Three input nodes
-		
+		#Input layer
+		#Three input nodes in one layer
 		@input_one = Neuron.new
 		@input_two = Neuron.new
 		@input_three = Neuron.new
+		@input_layer = Layer.new
 		
-		#The perceptron node with bias factor 0.4
-		@perceptron = Neuron.new(0.4)
+		@input_layer.add_neuron(@input_one)
+		@input_layer.add_neuron(@input_two)
+		@input_layer.add_neuron(@input_three)
 		
-		#The output node
+		@layers << @input_layer
+		
+		#Two nodes in a hidden_layer
+		@hidden_layer = Layer.new
+		@hidden_one = Neuron.new
+		@hidden_two = Neuron.new
+		@layers << @hidden_layer
+		
+		#Connections from input layer to hidden layer
+		@input_one_to_hidden_one = Axon.new(@input_one, @hidden_one, random_weight)
+		@input_one_to_hidden_two = Axon.new(@input_one, @hidden_two, random_weight)
+		@input_two_to_hidden_one = Axon.new(@input_two, @hidden_one, random_weight)
+		@input_two_to_hidden_two = Axon.new(@input_two, @hidden_two, random_weight)
+		@input_three_to_hidden_one = Axon.new(@input_three, @hidden_one, random_weight)
+		@input_three_to_hidden_two = Axon.new(@input_three, @hidden_two, random_weight)
+		
+		#Three nodes in the output layer
+		@output_layer = Layer.new
 		@output_one = Neuron.new
+		@output_two = Neuron.new
+		@output_three = Neuron.new
+		@layers << @output_layer
 		
-		#Three axons with weight 0.3 from inputs to perceptron
-		@input_one_to_p = Axon.new(@input_one, @perceptron, 0.3)
-		@input_two_to_p = Axon.new(@input_two, @perceptron, 0.3)
-		@input_three_to_p = Axon.new(@input_three, @perceptron, 0.3)
+		#Connections from hidden layer to output layer
+		@hidden_one_to_output_one = Axon.new(@hidden_one, @output_one, random_weight)
+		@hidden_one_to_output_two = Axon.new(@hidden_one, @output_two, random_weight)
+		@hidden_one_to_output_three = Axon.new(@hidden_one, @output_three, random_weight)
+		@hidden_two_to_output_one = Axon.new(@hidden_two, @output_one, random_weight)
+		@hidden_two_to_output_two = Axon.new(@hidden_two, @output_two, random_weight)
+		@hidden_two_to_output_three = Axon.new(@hidden_two, @output_three, random_weight)
 		
-		#One axon with weight 1.0 from perceptron to output
-		@p_to_output_one = Axon.new(@perceptron, @output_one, 1.0)
 	end
 	
 	
@@ -37,11 +71,13 @@ class ANN
 	end
 	
 	def evaluate
-		@perceptron.evaluate
-		#puts "Output of perceptron node was: #{@perceptron.get_value}"
+	
+		@layers.each do |layer|
+			layer.evaluate
+		end
 		
-		@output_one.evaluate
-		
-		return "Output was: #{@output_one.get_value}"
+		return "Output 1 was: #{@output_one.get_value}"
+		return "Output 2 was: #{@output_two.get_value}"
+		return "Output 3 was: #{@output_three.get_value}"
 	end
 end
